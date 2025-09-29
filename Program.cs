15c2af7b9e02be7951b1173cd41ed6eacefddb84
Guillaume Hamilton <h_guillaume@hotmail.com>
2025-09-29 18:01:15 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Create Google Gemini API Key Here: https://aistudio.google.com/app/api-keys
+// dotnet add package Google_GenerativeAI
+
+using GenerativeAI;
+using GoogleGeminiAPIKey;
+
+var apiKey = GoogleGeminiAPIKey.GoogleGeminiAPIKey.APIKey;
+
+var googleAI = new GoogleAi(apiKey);
+
+// List of Google Gemini models:  https://ai.google.dev/gemini-api/docs/models#gemini-2.0-flash
+var model = googleAI.CreateGenerativeModel("models/gemini-2.0-flash-001");
+//var model = googleAI.CreateGenerativeModel("models/gemini-2.5-flash");
+//var model = googleAI.CreateGenerativeModel("models/gemini-2.5-flash-lite");
+//var model = googleAI.CreateGenerativeModel("models/gemini-2.5-pro");
+
+var response = await model.GenerateContentAsync("Why is the sky blue?");
+Console.WriteLine(response.Text());
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GoogleGemini-CSharp
 Using Google Gemini AI in C#
+
+Create Google Gemini API Key Here: https://aistudio.google.com/app/api-keys
+
+Install the Google Generative AI NuGet package:
+```
+dotnet add package Google_GenerativeAI
+```
